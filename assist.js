51c5b0e899a77aedf4239bb6e1a145064645f529
Joker SMF <makerzzz.zzzz@gmail.com>
2013-02-07 15:41:01 +0530

    Added more functions

--- assist.js
+++ assist.js
@@ -17,6 +17,10 @@
 	};
 	assist.ajaxCallback = {};
 
+	var slice = Array.prototype.slice,
+	objHasProperty = Object.prototype.hasOwnProperty
+	objKeys = Object.keys;
+
 	assist.getType = function(obj) {
 		return toString.call(obj).toLowerCase();
 	};
@@ -127,15 +131,61 @@
 		if(assist.enableLog) console.log.apply(console, arguments);
 	};
 
-	assist.inArray = function(array, search) {
-		if(!assist.isArray(array)) return false;
-		else if (array.indexOf(search) != -1) return true;
-		return false
-	};
-
 	assist.intToFloat = function(num, decPlaces) {
 		return num.toFixed(decPlaces);
 	}
+
+	//all array related function
+	assist.inArray = function(array, search) {
+		if(!assist.isArray(array)) return false;
+		else if (array.indexOf(search) != -1) return true;
+		return false;
+	};
+
+	/*
+	 *can be used for array, multidimentional arrays (2d), strings
+	*/
+	assist.indexOf = function(data, search) {
+		if(assist.isArray(data))
+			for (var i = 0, z = data.length; i < z; i++) {
+				if (assist.isArray(data[i]) && data[i].indexOf(search) != -1) return i;
+				else if (assist.isString(data[i]) && data[i].indexOf(search) != -1) return i;
+				else if(data[i] == search) return i;
+			}
+		else if(assist.isString(data)) var a = data.indexOf(search); if(a != -1) return a;
+		return -1;
+	};
+
+	assist.lastArrayElem = function(input, num) {
+		if(!assist.isArray(input)) return input;
+		var len = input.length, range = Math.ceil(len - num);
+		if(num > len) return input;
+		if(!assist.isUndefined(num)) return slice.call(input, range)
+		else return input[len - 1]
+	}
+
+	/*
+	 * Functions related to objectss
+	 * return in form of array, nested objects are preserved
+	*/
+	assist.lastObjectElem = function(input, num) {
+		if(!assist.isObject(input)) return input;
+		delete input.length;
+		var oLen = assist.objLength(input),
+			range = Math.ceil(oLen - num);
+		if(num > oLen) return input;
+		if(!assist.objHasKey(input, 'lenght')) input.length = oLen;
+		if(!assist.isUndefined(num)) return slice.call(input, range);
+		else return slice.call(input, oLen - 1);
+	}
+
+	assist.objHasKey = function(obj, key) {
+		return objHasProperty.call(obj, key);
+	};
+
+	assist.objLength = function(obj) {
+		return objKeys(obj).length;
+	};
 
 	/*
 	 *options here contains - url, domId, success(), error(), cacheFile(false by default)
